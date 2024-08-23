db937298ec90edabebc073032e00301efd28476d
codemossaka <peyagodson@ymail.com>
2024-08-23 22:28:08 +0300

    output message edited

--- src/ru/godsonpeya/Main.java
+++ src/ru/godsonpeya/Main.java
@@ -8,6 +8,6 @@ public class Main {
         Person person2 = new Person(25);
 //        Person person3 = new Person("John");
 //        Person person4 = new Person("John", 25);
-        System.out.printf(" name %s et age %s",person2.getName(), person2.getAge());
+        System.out.printf(" name %s, age %s",person2.getName(), person2.getAge());
     }
 }
